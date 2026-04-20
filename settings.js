d14f1f6ccb31bf2ee713e25e7fa4116777034d8a
paxbarofficial <paxbarcommunity@gmail.com>
2026-04-20 11:22:12 +0800

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,10 @@
+global.owner = ["6283830646778"]
+global.version = "2.0.0"
+
+module.exports = { 
+    NAMA_OWNER: "barmodss",
+    NAMA_BOT: "PAXBAR V2",
+    NUMBER_BOT: "6283830646778",
+    WM: "SCRIPT PAXBAR V2",
+    PACNAME: "🦄 PAXBAR VERSION 2",
+};

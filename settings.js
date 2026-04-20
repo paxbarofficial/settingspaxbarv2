a01e5e4586582a01723ed40ea983dd9d39f4ea1a
paxbarofficial <paxbarcommunity@gmail.com>
2026-04-20 13:39:29 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 global.owner = ["6283830646778"]
 global.version = "𝟸.𝟶"
-global.session = "paxbarV2"
+global.session = "sessions"
 module.exports = { 
     NAMA_OWNER: "barmodss",
     NAMA_BOT: "PAXBAR V2",
